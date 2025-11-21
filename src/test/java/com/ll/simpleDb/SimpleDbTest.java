f3e951216296b9039518d62b8bd4fd6addca85d7
kimdohwan7220 <dohwan8220@naver.com>
2025-11-21 10:13:55 +0900

    TDD - 2 : R

--- src/test/java/com/ll/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -7,6 +7,19 @@ public class SimpleDbTest {
     private static SimpleDb simpleDb;
 
     private static void createArticleTable() {
+        simpleDb.run("DROP TABLE IF EXISTS article");
+
+        simpleDb.run("""
+                CREATE TABLE article (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    PRIMARY KEY(id),
+                    createdDate DATETIME NOT NULL,
+                    modifiedDate DATETIME NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    `body` TEXT NOT NULL,
+                    isBlind BIT(1) NOT NULL DEFAULT 0
+                )
+                """);
     }
 
     @BeforeAll
