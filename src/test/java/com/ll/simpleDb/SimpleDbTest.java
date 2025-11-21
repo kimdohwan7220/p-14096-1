3336de48d902e765afed5dfe0ef7ce89cf3b7861
kimdohwan7220 <dohwan8220@naver.com>
2025-11-21 10:10:30 +0900

    TDD - R

---
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -0,0 +1,21 @@
+package com.ll.simpleDb;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SimpleDbTest {
+    private static SimpleDb simpleDb;
+
+    @BeforeAll
+    public static void beforeAll() {
+        simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
+        simpleDb.setDevMode(true);
+
+        createArticleTable();
+    }
+
+    @Test
+    void t1() {
+
+    }
+}
